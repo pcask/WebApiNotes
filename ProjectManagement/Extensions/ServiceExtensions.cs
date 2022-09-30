ff14d6e0dd40927cff7235a562cc93854ca38dd4
sezer <sezer.ayran@gmail.com>
2022-09-30 14:19:42 +0300

    Create an extension method for CORS configurations

---
+++ ProjectManagement/Extensions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder
+                    .AllowAnyOrigin()   // Api'a başvuru yapan bütün origin'lere,
+                    .AllowAnyMethod()   // bütün method'lara
+                    .AllowAnyHeader()  // bütün http header'lara izin ver, demiş oluyoruz.
+                    .WithOrigins("www.google.com", "www.youtube.com")   // Veya sadece erişimine izin vermek istediğimiz origins, methods, headers' ları belirtebiliriz.
+                    .WithMethods("get");
+                });
+            });
+        }
+    }
+}
--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using ProjectManagement.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Yazmış olduğumuz extension method aracılığıyla Cors Configurations' larımızı Services'lara ekliyoruz.
+builder.Services.ConfigureCors();
 
 var app = builder.Build();
 
@@ -22,5 +27,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Services'lara eklemiş olduğumuz Cors Configurations' ları kullanacağımızı uygulamamıza belirtiyoruz.
+app.UseCors("CorsPolicy");
 
 app.Run();
